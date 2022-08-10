31817f1189c563c22724745bb56b07902e62eda4
CuriousBoy <40873994+490273789@users.noreply.github.com>
2022-08-10 16:51:33 +0800

    chore: commitlit file add note

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -9,3 +9,53 @@ module.exports = {
 // perf: 性能方面的优化。
 // refactor: 代码重构。
 // test: 添加一些测试代码等等。
+// types: [
+//   {
+//     value: ':sparkles: feat',
+//     name: '✨ feat:     新功能',
+//   },
+//   {
+//     value: ':bug: fix',
+//     name: '🐛 fix:      修复bug',
+//   },
+//   {
+//     value: ':tada: init',
+//     name: '🎉 init:     初始化',
+//   },
+//   {
+//     value: ':pencil2: docs',
+//     name: '✏️  docs:     文档变更',
+//   },
+//   {
+//     value: ':lipstick: style',
+//     name: '💄 style:    代码的样式美化',
+//   },
+//   {
+//     value: ':recycle: refactor',
+//     name: '♻️  refactor: 重构',
+//   },
+//   {
+//     value: ':zap: perf',
+//     name: '⚡️ perf:     性能优化',
+//   },
+//   {
+//     value: ':white_check_mark: test',
+//     name: '✅ test:     测试',
+//   },
+//   {
+//     value: ':rewind: revert',
+//     name: '⏪️ revert:   回退',
+//   },
+//   {
+//     value: ':package: build',
+//     name: '📦️ build:    打包',
+//   },
+//   {
+//     value: ':rocket: chore',
+//     name: '🚀 chore:    构建/工程依赖/工具',
+//   },
+//   {
+//     value: ':construction_worker: ci',
+//     name: '👷 ci:       CI related changes',
+//   },
+// ],
