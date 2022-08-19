a40843e75e6611d7f1b8c7b77bb9f9761e403818
CuriousBoy <40873994+490273789@users.noreply.github.com>
2022-08-19 11:20:26 +0800

    style: add global style

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
+import 'virtual:svg-icons-register'
+import './style/index.scss'
 import store from './store'
 import App from './App'
-import 'virtual:svg-icons-register'
-import './style/reset.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
